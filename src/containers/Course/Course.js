d9c1a21fb4f15503b9c51e9473eb9655a6574aa3
Viktor Sapozhnyk <victor_sp@ukr.net>
2018-06-27 18:48:11 +0300

    init commit

---
+++ src/containers/Course/Course.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Course extends Component {
+    state = {
+        tittle: 'NO TITTLE'
+    }
+
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search);
+        for (let param of query.entries()) {
+            console.log(param); // yields ['start', '5']
+            if(param[0] == 'tittle') {
+                this.setState({tittle: param[1]})
+            }
+        }
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>{this.state.tittle}</h1>
+                <p>You selected the Course with ID: {this.props.match.params.id}</p>
+            </div>
+        );
+    }
+}
+
+export default Course;
